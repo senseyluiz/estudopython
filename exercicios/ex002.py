0352e8f1c9d6d91c8172b9447cb8e33bdb950428
senseyluiz <senseyluiz@gmail.com>
2019-12-12 21:11:06 -0200

    exercicios 2 e 3

---
+++ exercicios/ex002.py
@@ -0,0 +1,5 @@
+# Exercício Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input("Digite seu nome: ")
+
+print (f"Bem vindo ao PYTHON {nome}!")
